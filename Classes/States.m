dd96901374cfc379f33a9ef36421998d2e5a269f
beepscore <support@beepscore.com>
2010-01-18 16:57:11 -0800

    Implement statesArray with names of states.

--- Classes/States.m
+++ Classes/States.m
@@ -15,14 +15,31 @@
 - (id)init {
     self = [super init];
     if (self) {
+        statesArray = [[NSMutableArray alloc] init];
+
         // path is autoreleased  Don't retain it.
         NSString *path=[[NSBundle mainBundle] pathForResource:@"data" ofType:@"plist"];
         
-        // TODO: Get only the top level values, not the whole dictionary
-        self.statesArray = [[NSDictionary alloc] initWithContentsOfFile:path];
-        // [self.statesArray count] = 19, note the last one is not a state
+        NSDictionary *tempDict;
+        tempDict = [[NSDictionary alloc] initWithContentsOfFile:path];
+        
+        for(NSString *aState in tempDict){
+            [self.statesArray addObject:aState];
+        }
+        [tempDict release];        
+ 
+        // Sort statesArray alphabetically
+        [self.statesArray sortUsingSelector:@selector(compare:)];
+        
+        // TODO: Get only the top level values, not the whole dictionary        
+        // [tempDict objectForKey:@"Alabama"];
+        // ????: can do plist objectForKey:@"Alabama"  ???????
+        // [tempDict valueForKey:aKey];
+        
+        // [self.statesArray count] = 19, note one is not a state
         NSLog(@"statesArray count = %d", [self.statesArray count]);
         NSLog(@"statesArray = %@", self.statesArray);
+
     }
     return self;
 }
